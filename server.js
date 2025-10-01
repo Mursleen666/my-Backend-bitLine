5ef285ab146d352255fb062f8633d70b45a22371
M.Mursleen <mursleenmamoon@gmail.com>
2025-10-02 01:32:10 +0500

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.get("/", (req, res) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`✅ Server running on port ${PORT}`);
 });
